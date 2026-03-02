69e5875e1f1b402a546bf1a7df6929c844420e9b
sebastiangf03 <126507418+sebastiangf03@users.noreply.github.com>
2026-03-02 12:03:29 +0100

    Create README.d

---
+++ data/data_modificada/README.d
@@ -0,0 +1,10 @@
+# 📂 Carpeta de Datos
+
+Este directorio contiene la estructura necesaria para la ejecución del análisis estadístico.
+
+### 🔒 Confidencialidad
+Siguiendo las políticas de privacidad y confidencialidad mencionadas en el proyecto, los archivos originales (.txt) y procesados (.csv) de los 450 tickets no se encuentran alojados en este repositorio público.
+
+### 🛠️ Estructura:
+- [cite_start]`data/`: Contiene los archivos de texto bruto extraídos de los PDF.
+- `data/data_modificada/`: Contiene los archivos procesados por el script de Python listos para ser importados en R.
